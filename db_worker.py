d35bd801a7b32acdaf043921b92c28988ce1bfc7
rishm069 <rishm069@protonmail.com>
2020-01-26 23:13:02 +0800

    db_worker updated

--- db_worker.py
+++ db_worker.py
@@ -7,18 +7,22 @@ try:
                                          user='solar',
                                          password='solar')
 
+    nmbr = 1010
+    
     sql_select_Query = "SELECT * FROM solar_swpc WHERE Nmbr = " + nmbr + " ORDER BY Date;"
     cursor = connection.cursor()
     cursor.execute(sql_select_Query)
     records = cursor.fetchall()
-    print("Total number of rows in Laptop is: ", cursor.rowcount)
+    print("Total number of days the spot presented: ", cursor.rowcount)
 
     print("\nPrinting each laptop record")
+    
     for row in records:
-        print("Id = ", row[0], )
-        print("Name = ", row[1])
-        print("Price  = ", row[2])
-        print("Purchase date  = ", row[3], "\n")
+        
+        print("Date = ", row[0], )
+        print("Nmbr = ", row[1])
+        print("Area  = ", row[4])
+        
 
 except Error as e:
     print("Error reading data from MySQL table", e)
